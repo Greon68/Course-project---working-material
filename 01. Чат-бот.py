05ccdde83a29ee51f8aadf03d86110eb8c80ac3a
Greon68 <greon-ya@yandex.ru>
2023-08-19 00:43:21 +0300

    initial commit-18.08.23

--- 01. Чат-бот.py
+++ 01. Чат-бот.py
@@ -6,124 +6,190 @@ from vk_api import VkUpload
 from tqdm import tqdm
 from pprint import pprint
 import random
+from vk_api.keyboard import  VkKeyboard , VkKeyboardColor
 
 
-
-# token = input('Token: ')
-token = ''
-
-
-# Класс для работы с API ВК
-class VkUser:
-    url = 'https://api.vk.com/method/'
-
-    def __init__(self, token, version):
-        self.params = {
-            'access_token': token,
-            'v': version
-        }
-
-    def get_users(self, user_id=None):
-        ''' Функция возвращает данные пользователя по его id'''
-        url = self.url + 'users.get'
-        users_params = {
-            'user_id': user_id,
-            'fields': 'education,city_name, sex,has_photo,photo_max_orig'
-        }
-        res = requests.get(url, params={**self.params, **users_params}).json()
-        return res
-        # pprint(res)
-
-
-
-    def get_name_user(self,user_id=None):
-        ''' Функция возвращает имя пользователя по его id '''
-        self.user_id = user_id
-        data_json = self.get_users(user_id)
-        name_user = data_json['response'][0]['first_name']
-        return name_user
-        # print(name_user)
+# Токен сообщества для чат-бота:
+token_group = ' '
+# token_group = input('Token: ')
 
 
 
 
+vk_bot = vk_api.VkApi(token=token_group) # АВТОРИЗАТОР ( сессия )
+longpoll = VkLongPoll(vk_bot) # Переменная для работы с API типа LongPoll
 
-vk_client = VkUser(token, '5.131')
+# Переменная - загрузчик изображений
+uploader = VkUpload(vk_bot)
 
-
+# СОЗДАНИЕ КЛАВИАТУРЫ (КНОПОК)
+# Создадим переменную , содержащую вызов клавитатуры:
+# one_time= True - одноразовая клавиаиура
+# one_time= False - многоразовая клавиатура
+keyboard = VkKeyboard(one_time= True)
+# Пропишем кнопки :
+keyboard.add_button('Начать',color=VkKeyboardColor.POSITIVE)
+# keyboard.add_line() # делает кнопку на всю ширину поля сообщения
+keyboard.add_button('Новый поиск', color=VkKeyboardColor.POSITIVE)
+keyboard.add_line()
+keyboard.add_button('Фото', color=VkKeyboardColor.NEGATIVE)
+keyboard.add_line()
+# Кнопка для перехода по ссылке
+keyboard.add_openlink_button('Кнопка со ссылкой', link = 'https://vk.com')
 
 # адрес изображения для загрузки
-image = 'good_day.jpg'
+image = 'D:/ФОТО/good_day.jpg'
 image_list = ['Foto/photo-0.jpg','Foto/photo-1.jpg','Foto/photo-2.jpg','Foto/photo-3.jpg' ]
 perplexity_photo_list = ['удивление.jpg','не_понимаю.jpeg','не_понимаю-2.jpg']
 
-authorize = vk_api.VkApi(token=token) # АВТОРИЗАТОР ( сессия )
-longpoll = VkLongPoll(authorize) # Переменная для работы с API типа LongPoll
-# Переменная - загрузчик изображений
-uploader = VkUpload(authorize)
+
 
 
 def write_msg(user_id, message=None ,attachment=None):
     '''Функция для отправки сообщения'''
-    authorize.method('messages.send', {'user_id': user_id, 'message': message,'attachment':attachment, 'random_id': randrange(10 ** 7)})
-    # authorize.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7), 'attachment':attachment})
+    vk_bot.method('messages.send',
+                  {'user_id': user_id,
+                   'message': message,
+                   'attachment':attachment,
+                   'keyboard': keyboard.get_keyboard(),
+                   'random_id': randrange(10 ** 7)})
+
 
 def upload_image(photos,user_id, message=None ):
     '''Функция отправки изображений.
     Здесь : photos - путь к фотографии на локальном компьютере,
-     message- текст сообщения (не обязательный параметр)
-     Внутри себя вызывает функцию отправки сообщений write_msg()'''
+     message- текст сообщения (не обязательный параметр)'''
     # Загрузчик фотографий.
     # Результат запроса - список со словарём внутри
     # Избавляемся от списка , оставляем словарь
-    upload_dict = uploader.photo_messages(photos)[0]
-    pprint(upload_dict) # Для проверки
+    upload_dict= uploader.photo_messages(photos)[0]
+    # pprint(upload_dict)
     # Вытаскиваем из словаря необходимые параметры
     owner_id = upload_dict["owner_id"]
     media_id = upload_dict["id"]
     # Аргумент для загрузки фото:
     attachment = f'photo{owner_id}_{media_id}'
-    print(attachment) # photo-221946829_456239071
+    print(f'Параметры загруженного изображения - {attachment}') # photo-221946829_456239071
     # Загружаем изображение с помощью функции write_msg()
     write_msg(user_id, message, attachment)
 
-# Функция longpoll.listen() 'слушает' сервер ВКонтакте ,
-# event - сообщение , пришедшее на сервер (== в чат группы )
-# Используем прогресс-бар tqdm для контроля работы кода
-for event in tqdm(longpoll.listen()):
-    # Сравниваем тип пришедшего сообщения с тем , что мы ожидаем ,
-    # есть ли такой тип в библиотеке vk_api, а , именно , в её подразделе VkEventType ,
-    # т.е. проверяем соответствует ли полученное событие шаблонам события получения сообщений
-    if event.type == VkEventType.MESSAGE_NEW :
+def get_user_name(user_id):
+    '''Получаем имя пользователя по его id ,
+     используя метод users.get '''
+    user_name_list = vk_bot.method('users.get',{'user_ids':user_id , 'fields': 'first_name' })
+    # pprint(user_name_list)
+    user_name = user_name_list[0]['first_name']
+    return user_name
+
+def random_number():
+    number = random.randint(0,10)
+    return number
+
+def random_number_2(message):
+    end = int(message)
+    number = random.randint(0,end+1)
+    return number
+
+def chatbot():
+    # Функция longpoll.listen() 'слушает' сервер ВКонтакте ,
+    # event - сообщение , пришедшее на сервер (== в чат группы )
+    # Используем прогресс-бар tqdm для контроля работы кода
+    for event in tqdm(longpoll.listen()):
+        # Сравниваем тип пришедшего сообщения с тем , что мы ожидаем ,
+        # есть ли такой тип в библиотеке vk_api, а , именно , в её подразделе VkEventType ,
+        # т.е. проверяем соответствует ли полученное событие шаблонам события получения сообщений
         # Проверяем , кому именно адресовано событие ( сообщение ) -
         # действительно ли сообщение адресовано нашему боту
-        if event.to_me:
-            # Проверяем , является ли полученнное сообщение текстом ( функция event.text),
-            # и полученный текст сохраняем в переменную request :
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            # Проверяем , кому именно адресовано событие ( сообщение ) -
+            # действительно ли сообщение адресовано нашему боту
             messages_input = event.text
+            print()
+            print(f'Сообщение от пользователя - {messages_input}')
             # Запишем в переменную идентификатор собеседника :
             user_id = event.user_id
+            print(f'user_id - {user_id}')
 
             # Опрелелим  имя пользователя по его user_id ,
             # вызвав метод get_name_user() класса VkUser
-            name_user = vk_client.get_name_user(user_id)
+            # name_user = vk_client.get_name_user(user_id)
+            name_user = get_user_name(user_id)
 
-            # Диалоги
             if  messages_input.lower() == "начать" :
                 upload_image('чат-бот.png', user_id,f'Привет, {name_user}! Я - Чат-Бот. Буду рад пообщаться !!!')
-                write_msg(user_id,'Я умею реагировать на слова "привет" , "пока" и на любые другие сообщения .\n'                                 
-                                  'Захочешь получить несколько красивых фотографий , напиши слово "фото".\n'
-                                  ' Если хочешь , чтобы я подобрал для тебя собеседников , пришли сообщение со словом "старт" ', attachment=None)
+                write_msg(user_id,' Если хочешь , чтобы я подобрал для тебя собеседников , '
+                                  'пришли сообщение со словом "старт" или  нажми кнопку "Новый поиск" :\n'
+                                  '1 - получение случайного числа ;\n'
+                                  '2 - вводим 4 требуемых параметра', attachment=None)
             elif messages_input.lower() == "привет" :
                 upload_image(image, user_id,f"Привет, {name_user}! ")
+
             elif messages_input.lower() == "пока":
+                # write_msg(user_id, f"Пока, {name_user}. Заходи ещё !")
                 upload_image('пока.jpg', user_id, f"До свидания, {name_user}!")
+
             elif messages_input.lower() == "фото":
                 for img in image_list:
-                    upload_image(img,user_id,"Фото для Вас !" )
+                        upload_image(img,user_id,"Фото для Вас !" )
+
+            # Подучение случайного числа :
+            elif messages_input =='1':
+                # write_msg(user_id=user_id,message= random_number())
+                write_msg(user_id=user_id,message="Диапозон чисел от 0 до ...")
+                # Снова слушаем чат :
+                for event in longpoll.listen():
+                    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                        msg = event.text # То , что получили от пользователя
+                        user_id = event.user_id
+                        # Проверяем , является ли полученное сообщение - числом :
+                        try:
+                            msg = int(msg)
+                            write_msg(user_id=user_id, message=random_number_2(msg))
+                            # break
+                        except:
+                            write_msg(user_id=user_id, message="Введено не число")
+                            break
+
+
+            # Получение набора параметров в диалоге от пользователя:
+            # НЕ РАБОТАЕТ - НЕ ВЫХОДИТ ИЗ ЦИКЛА
+            elif messages_input == '2':
+                write_msg(user_id=user_id,message="Введите название города")
+                for event in longpoll.listen():
+                    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                        sity = event.text # Название города
+                        write_msg(user_id=user_id, message="Введите пол собеседника (м/ж)")
+                        for event in longpoll.listen():
+                            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                                sex = event.text.lower() # Пол собеседника
+
+                                write_msg(user_id=user_id, message="Введите нижнюю границу возраста")
+                                for event in longpoll.listen():
+                                    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                                        age_from = event.text  # Нижняя граница возраста
+
+                                        write_msg(user_id=user_id, message="Введите верхнюю границу возраста")
+                                        for event in longpoll.listen():
+                                            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                                                age_to = event.text  # Нижняя граница возраста
+                                                user_id = event.user_id
+                                                try:
+                                                    age_to = int(age_to)
+                                                    write_msg(user_id=user_id, message=f"Вы ввели следующие данные: \n"
+                                                     f"Город - {sity} , пол собеседника - {sex}, возраст : от {age_from} до {age_to} лет ")
+                                                    # break
+                                                except:
+                                                    write_msg(user_id=user_id, message="Введено не число")
+                                                # print(sity,sex,age_from,age_to)
+                                                    break
+
+
+
             else:
+                # write_msg(user_id, "Не понял Ваш вопрос...")
                 # Рандомная загрузка фото из списка :
                 upload_image(random.choice(perplexity_photo_list), user_id, "Не понял Ваш вопрос...")
 
-
+# Запускаю в работу :
+chatbot()
 
---
+++ VK _API.py
@@ -0,0 +1,267 @@
+from pprint import pprint
+import requests
+import json
+from datetime import timedelta
+from ratelimit import limits, sleep_and_retry
+from tqdm import tqdm
+import random
+# Токен ВК
+token = ' '
+
+
+
+# Класс для работы с API ВК
+class VkUser :
+    url = 'https://api.vk.com/method/'
+    def __init__(self,token,version):
+        self.params = {
+            'access_token': token,
+            'v': version
+        }
+
+    def get_users(self,user_id=None):
+        url =self.url + 'users.get'
+        users_params = {'user_ids':user_id,
+                  'fields': 'sex, bdate  '
+                  }
+        res = requests.get(url, params={**self.params, **users_params}).json()
+        return res
+        # pprint(res)
+
+    def users_search_2(self,hometown=None,age_from=None,age_to=None, sex=None):
+        url =self.url + 'users.search'
+        users_params = {
+            'count':10,
+            'hometown':hometown ,
+            'sex': sex ,
+            'sort':0,
+            'age_from':age_from ,
+            'age_to': age_to,
+
+            'fields': 'country,city, sex , bdate , domain '
+            }
+        res = requests.get(url, params={**self.params, **users_params}).json()
+        # Избавимся от вложенности , создадим список с параметрами пользователей :
+        users_info_list_all = res['response']['items']
+        # pprint(users_info_list_all)
+        # Отфильтруем лишние города и закрытые профили :
+        users_info_list = []
+        for user in users_info_list_all :
+            if 'city' in user and user['city']['title'] == hometown and user['is_closed'] is False :
+                users_info_list.append(user)
+        # pprint(users_info_list)
+        # # Получим список с id пользователей :
+        users_id_list = []
+        for user in users_info_list:
+            user_id = user['id']
+            users_id_list.append(user_id )
+        # print (len(users_id_list),users_id_list)
+        return [users_info_list, users_id_list]
+
+    def users_search_3(self,hometown=None,age_from=None,age_to=None, sex=None):
+        url =self.url + 'users.search'
+        users_params = {
+            'count':1000,
+            'hometown':hometown ,
+            'sex': sex ,
+            'sort':0,
+            'age_from':age_from ,
+            'age_to': age_to,
+
+            'fields': 'country,city, sex , bdate , domain '
+            }
+        res = requests.get(url, params={**self.params, **users_params}).json()
+        # Избавимся от вложенности , создадим список с параметрами пользователей :
+        users_info_list_all = res['response']['items']
+        # pprint(users_info_list_all)
+        # Отфильтруем лишние города и закрытые профили :
+        users_info_list = []
+        for user in users_info_list_all :
+            if 'city' in user and user['city']['title'] == hometown and user['is_closed'] is False :
+                users_info_list.append(user)
+
+        return users_info_list
+
+    # Работаем с фотографиями . Запрос на photos.get
+
+    # @sleep_and_retry
+    # @limits(calls=2, period=timedelta(seconds=1).total_seconds())
+    def get_best_photos_2 (self,user_id=None,count=100):
+        ''' Функция возвращает список из 3-х самых популярных по лайкам
+        фотографий со стены пользователя. Используем декораторы
+        @sleep_and_retry и @limits(calls=2, period=timedelta(seconds=1).total_seconds()),
+        ограничивающие количество запросов в секунду.
+        В данном случае отправляем 2 запроса в 1 секунду
+        '''
+        photos_url = self.url + 'photos.get'
+        photos_params = {
+            'owner_id': user_id ,
+            'album_id': 'wall',
+            'rev': 1 ,
+            'extended': 1 ,
+            'photo_sizes': 1,
+            'count': count
+        }
+        res = requests.get(photos_url, params={**self.params, **photos_params}).json()
+        # pprint(res)
+
+        # Избавляемся от закрытых профилей и профилей без фотографий:
+        if 'response' in res and res ["response"]["count"]>0:
+            # pprint (res)
+
+            # Находим фото максимального размера.
+            # Вытягиваем в отдельный файл блок "items":
+            fotos_items_list = res ["response"]["items"]
+            # pprint(fotos_items_list)
+            size_dict = {'s': 1, 'm': 2, 'o': 3, 'p': 4, 'q': 5, 'r': 6, 'x': 7, 'y': 8, 'z': 9, 'w': 10}
+            data_photos_list = []
+            for data in fotos_items_list :
+                likes = data['likes']['count']
+                size_max=0
+                for element in data['sizes']:
+                    letter_size_foto = element['type']
+                    # Проверка на наличии обозначения размера фото в контрольном словаре размеров:
+                    if letter_size_foto in size_dict.keys():
+                      # Ищем фото с максимальным размером , сохраняем соответствующую букву размера (letter_max_foto)
+                        # и ссылку на фото максимального размера (url_foto)
+                        if size_dict[letter_size_foto] > size_max:
+
+                            size_max = size_dict[letter_size_foto]
+                            url_foto = element['url']
+                    else:
+                         print(f'Ошибка. Размера "{letter_size_foto}" нет в контрольном списке размеров фотографий')
+
+                # Сохраняем данные о выбранном фото в текущем словаре:
+                element_dict={
+                              'likes' :  likes ,
+                              'url_foto':url_foto
+                              }
+                # Добавляем текущий словарь в общий список из словарей с данными о выбранных фото:
+                data_photos_list.append(element_dict)
+                # Сортируем список по количеству лайков :
+                data_photos_list_sort = sorted(data_photos_list, key=lambda x: x['likes'],reverse= True)
+                # print(len(data_photos_list_sort))
+                # pprint(data_photos_list_sort)
+
+                # Выбираем 3 фото с максимальным количеством лайков:
+                best_photos_list = data_photos_list_sort[:3]
+
+                # Вытаскиваем адреса фотографий :
+                best_photos_list_url= []
+                for photo in best_photos_list:
+                    best_photos_list_url.append(photo['url_foto'])
+            return best_photos_list_url
+
+
+    def get_companions_all (self,hometown=None,age_from=None,age_to=None, sex=None):
+        ''' Функция получает на вход параметры поиска пользователя.
+        Возвращает список со словарями данных найдённых пользователей '''
+        # Посмотрим  , сколько всего анкет мы имеем для обработки (для контроля) :
+        users_id_list = self.users_search_2(hometown, age_from, age_to, sex)[1]
+        print(f' Найдено {len(users_id_list)} анкет со следующими id : {users_id_list}')
+        # Получаем расширенную информацию о пользователях :
+        users_info_list= self.users_search_2(hometown,age_from,age_to, sex)[0]
+        # print(len(users_info_list))
+        # pprint(users_info_list)
+        #
+        # Формируем пустой список для вывода финишной информации :
+        user_list = []
+
+        # Работаем с каждой анкетой из общего списка:
+        for user in tqdm(users_info_list):
+            user_dict = {}
+            user_id = user['id']
+            # Получаем 3 фотографии нужного пользователя
+            photos_url_list = vk_client.get_best_photos_2(user_id=user_id)
+            # pprint (photos_url_list)
+            # Наполняем текущий словарь требуемыми значениями .
+            if photos_url_list is not None  :
+                user_dict['bdate'] = user['bdate']
+                user_dict['city'] = user['city']['title']
+                user_dict['domain'] = user['domain']
+                user_dict['photos_url_list'] = photos_url_list
+                user_dict['first_name'] = user['first_name']
+                user_dict['last_name'] = user['last_name']
+                user_dict['sex'] = user['sex']
+                user_dict['id'] = user['id']
+                user_list.append(user_dict)
+        return user_list
+
+    def user_profile(self):
+        my_list = (input('Введите список параметров -')).split(",")
+        # print (my_list)
+        start = 'да'
+        while len(my_list):
+            if start.lower() == 'да':
+                el = my_list.pop(0)
+                print(el)
+                # print(a_list)
+                start = input('Для просмотра следующего элемента списка напишите "да" - ')
+            else:
+                print("Вы вышли из режима просмотра анкет")
+                break
+        else:
+            print()
+            print('Список обработанных анкет законился.\n'
+                  'Введите новые параметры поиска')
+
+    def get_companion (self,hometown=None,age_from=None,age_to=None, sex=None):
+        '''Функция получает на вход параметры поиска ,
+         возвращает данные одного собеседника '''
+        # Получаем расширенную информацию о пользователях :
+        users_info_list = self.users_search_3(hometown, age_from, age_to, sex)
+        # pprint(users_info_list)
+        print(f'Получено {len(users_info_list)} анкет(ы,а) ')
+        # Работаем с каждой анкетой индивидуально:
+        # user_info = users_info_list.pop(0)
+        start = 'да'
+        while len(users_info_list):
+            if start.lower() == 'да':
+                # Рандомный выбор из списка с удалением
+                # test_list = [6, 4, 8, 9, 10]
+                # x = test_list.pop(random.randrange(len(test_list)))
+                user_info = users_info_list.pop(random.randrange(len(users_info_list)))
+                # user_info = users_info_list.pop(0)
+                # pprint(user_info)
+                user_dict = {}
+                user_id = user_info['id']
+                # Получаем 3 фотографии нужного пользователя
+                photos_url_list = vk_client.get_best_photos_2(user_id=user_id)
+                # pprint (photos_url_list)
+                # Наполняем текущий словарь требуемыми значениями .
+                if photos_url_list is not None:
+                    user_dict['bdate'] = user_info['bdate']
+                    user_dict['city'] = user_info['city']['title']
+                    user_dict['domain'] = user_info['domain']
+                    user_dict['photos_url_list'] = photos_url_list
+                    user_dict['first_name'] = user_info['first_name']
+                    user_dict['last_name'] = user_info['last_name']
+                    user_dict['sex'] = user_info['sex']
+                    user_dict['id'] = user_info['id']
+                pprint(user_dict)
+                # Сохранение изображений из списка ссылок
+                count = 0
+                for url in tqdm(photos_url_list):
+                    count += 1
+                    name = f'photo-{count}'
+                    res = requests.get(url)
+                    with open(f'Foto_user/{name}.jpg', 'wb') as file:
+                        file.write(res.content)
+
+                start = input('Для просмотра следующего элемента списка напишите "да" - ')
+            else:
+                print("Вы вышли из режима просмотра анкет")
+                break
+        else:
+            print()
+            print('Список обработанных анкет законился.\n'
+                  'Введите новые параметры поиска')
+
+
+# Работаем с ВК:
+
+# Инициализируем объект класса VkUser :
+vk_client = VkUser(token, '5.131')
+
+
+
